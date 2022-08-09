c730df363cfa5f4f115b2299f9cb137f71c67d71
Bryan Meadows <bryan1993meadows@gmail.com>
2022-08-08 19:18:03 -0600

    commit tree db2c63f110c99c77776bbd4d7dc0fae27c14cdfc

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(declarations = 'roller-skate'){
+    return `This Saturday, I want to ${declarations}!`;
+}
+function mondayWork(def = 'go to the office'){
+    return `This Monday, I will ${def}.`
+}
+function wrapAdjective(result){
+   return function(para = 'special'){
+    return `You are ${result}${para}${result}!`
+   }
+}
